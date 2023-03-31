eba0f2da7a8290bb203b7bcbcdaf470f53bc0434
bobrbase <11092002nikolai@gmail.com>
2023-03-31 15:40:32 +0300

    push me

---
+++ intensif.sql
@@ -0,0 +1,281 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 31 2023 г., 15:10
+-- Версия сервера: 8.0.30
+-- Версия PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `intensif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `forms`
+--
+
+CREATE TABLE `forms` (
+  `id` int NOT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fields` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `intensives`
+--
+
+CREATE TABLE `intensives` (
+  `id` int NOT NULL,
+  `name` int NOT NULL,
+  `decription` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lector_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `intensives_thematics`
+--
+
+CREATE TABLE `intensives_thematics` (
+  `intensive_id` int NOT NULL,
+  `thematic_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int NOT NULL,
+  `title` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int NOT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `decsription` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start_time` timestamp NOT NULL,
+  `end_time` timestamp NOT NULL,
+  `intensive_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `thematics`
+--
+
+CREATE TABLE `thematics` (
+  `id` int NOT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` int NOT NULL,
+  `email` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_forms_intensives`
+--
+
+CREATE TABLE `users_forms_intensives` (
+  `user_id` int NOT NULL,
+  `form_id` int NOT NULL,
+  `intensive_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_intensives`
+--
+
+CREATE TABLE `user_intensives` (
+  `user_id` int NOT NULL,
+  `intensive_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `forms`
+--
+ALTER TABLE `forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `intensives`
+--
+ALTER TABLE `intensives`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lector_id` (`lector_id`);
+
+--
+-- Индексы таблицы `intensives_thematics`
+--
+ALTER TABLE `intensives_thematics`
+  ADD KEY `intensive_id` (`intensive_id`),
+  ADD KEY `thematic_id` (`thematic_id`);
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `intensive_id` (`intensive_id`);
+
+--
+-- Индексы таблицы `thematics`
+--
+ALTER TABLE `thematics`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users_forms_intensives`
+--
+ALTER TABLE `users_forms_intensives`
+  ADD KEY `form_id` (`form_id`),
+  ADD KEY `intensive_id` (`intensive_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `user_intensives`
+--
+ALTER TABLE `user_intensives`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `intensive_id` (`intensive_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `forms`
+--
+ALTER TABLE `forms`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `intensives`
+--
+ALTER TABLE `intensives`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `thematics`
+--
+ALTER TABLE `thematics`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `intensives`
+--
+ALTER TABLE `intensives`
+  ADD CONSTRAINT `intensives_ibfk_1` FOREIGN KEY (`lector_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `intensives_thematics`
+--
+ALTER TABLE `intensives_thematics`
+  ADD CONSTRAINT `intensives_thematics_ibfk_1` FOREIGN KEY (`intensive_id`) REFERENCES `intensives` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `intensives_thematics_ibfk_2` FOREIGN KEY (`thematic_id`) REFERENCES `thematics` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `schedules`
+--
+ALTER TABLE `schedules`
+  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`intensive_id`) REFERENCES `intensives` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `users_forms_intensives`
+--
+ALTER TABLE `users_forms_intensives`
+  ADD CONSTRAINT `users_forms_intensives_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `users_forms_intensives_ibfk_2` FOREIGN KEY (`intensive_id`) REFERENCES `intensives` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `users_forms_intensives_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `user_intensives`
+--
+ALTER TABLE `user_intensives`
+  ADD CONSTRAINT `user_intensives_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `user_intensives_ibfk_2` FOREIGN KEY (`intensive_id`) REFERENCES `intensives` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
